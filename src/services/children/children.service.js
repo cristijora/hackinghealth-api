e990293bf4d5d4a8c2d731d9a2c64460c91a4749
cristian.jora <cristian.jora@corebuild.eu>
2017-10-26 09:46:44 +0300

    Convert children to NeDB service Add db files

--- src/services/children/children.service.js
+++ src/services/children/children.service.js
@@ -1,23 +1,25 @@
 // Initializes the `children` service on path `/children`
-const createService = require('feathers-mongodb');
+const createService = require('feathers-nedb');
+const createModel = require('../../models/children.model');
 const hooks = require('./children.hooks');
 const filters = require('./children.filters');
 
 module.exports = function () {
   const app = this;
+  const Model = createModel(app);
   const paginate = app.get('paginate');
-  const mongoClient = app.get('mongoClient');
-  const options = { paginate };
+
+  const options = {
+    name: 'users',
+    Model,
+    paginate
+  };
 
   // Initialize our service with any options it requires
   app.use('/children', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('children');
-
-  mongoClient.then(db => {
-    service.Model = db.collection('children');
-  });
 
   service.hooks(hooks);
 
